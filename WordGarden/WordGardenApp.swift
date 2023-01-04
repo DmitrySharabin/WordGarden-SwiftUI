91ceef675fd21fb969edd931412436085af9684b
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-04 12:12:54 +0100

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Dmitry Sharabin on 4.1.23..
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
